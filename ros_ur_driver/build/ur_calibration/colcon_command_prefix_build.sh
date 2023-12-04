aa38cd1d5b94b01ecb5bb99175944fb0c4480fe9
PedroAcero <pj.acero@alumnos.upm.es>
2023-12-04 20:49:56 +0100

    Añadidos el repositorio que controla el movimiento del robot

---
+++ ros_ur_driver/build/ur_calibration/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/pedro/workspace/ros_ur_driver/install/ur_dashboard_msgs/share/ur_dashboard_msgs/package.sh"
+. "/home/pedro/workspace/ros_ur_driver/install/ur_description/share/ur_description/package.sh"
+. "/home/pedro/workspace/ros_ur_driver/install/ur_controllers/share/ur_controllers/package.sh"
+. "/home/pedro/workspace/ros_ur_driver/install/ur_robot_driver/share/ur_robot_driver/package.sh"
