adef06f211fa837acca5a90b030f4f05a3546925
AlvaroBarry <abarrigonfer@gmail.com>
2024-05-14 17:46:25 +0200

    micro_ros funcionando y comentado

--- Universal_Robots_ROS2_Driver/ur_bringup/launch/com_arduino.py
+++ Universal_Robots_ROS2_Driver/ur_bringup/launch/com_arduino.py
@@ -1,3 +1,12 @@
+#Programa que se suscribe al nodo "micro_ros_arduino_node", creado por una ESP32,
+#  e intercambia información, como temperatura objetivo del extrusor, si hay  que
+# imprimir o no, que temperatura tiene el extrusor, y si se esta imprimiendo o no.
+# Para entenderlo mejor ver el archivo micro-ros_publisher_prueba.ino en el paquete micro_ros, 
+# en la siguiente dirección:
+
+# /workspace/ros_ur_driver/src/micro_ros_arduino/examples/micro-ros_publisher_prueba/micro-ros_publisher_prueba.ino
+
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
@@ -5,29 +14,51 @@ from std_msgs.msg import Int32
 class HumbleNode(Node):
     def __init__(self):
         super().__init__('micro_ros_arduino_node')
-        self.publisher_temperatura = self.create_publisher(Int32, 'temperatura_objetivo', 10)
-        self.publisher_pasos = self.create_publisher(Int32, 'pasos_objetivo', 10)
+        self.publisher_temperatura_objetivo = self.create_publisher(Int32, 'temperatura_objetivo', 10)
+        self.publisher_temperatura_cama_objetivo = self.create_publisher(Int32, 'temperatura_cama_objetivo', 10)
+        self.publisher_imprimir_objetivo = self.create_publisher(Int32, 'imprimir_objetivo', 10)
+        self.publisher_longitud_extrusion_objetivo = self.create_publisher(Int32, 'longitud_extrusion_objetivo', 10)
+        
         self.subscription_temperatura = self.create_subscription(Int32, 'temperatura', self.temperatura_callback, 10)
-        self.subscription_pasos = self.create_subscription(Int32, 'pasos', self.pasos_callback, 10)
-        self.timer = self.create_timer(1, self.timer_callback)
-        self.temp_count = 200
-        self.pasos_count = 5
+        self.subscription_temperatura_cama = self.create_subscription(Int32, 'temperatura_cama', self.temperatura_cama_callback, 10)
+        self.subscription_imprimir = self.create_subscription(Int32, 'imprimir', self.imprimir_callback, 10)
+        self.timer = self.create_timer(0.1, self.timer_callback)
+        
+#DATOS QUE SE QUIEREN ENVIAR ----------------------------------------------------------------------------------------------------------    
+        
+        self.temperatura_objetivo = 200
+        self.temperatura_cama_objetivo = 60
+        self.imprimir_objetivo = 1
+        self.longitud_extrusion_objetivo = 1000
+        
+#--------------------------------------------------------------------------------------------------------------------------------------
 
     def timer_callback(self):
         temperatura_msg = Int32()
-        pasos_msg = Int32()
-        temperatura_msg.data = self.temp_count
-        pasos_msg.data = self.pasos_count
-        self.publisher_temperatura.publish(temperatura_msg)
-        self.publisher_pasos.publish(pasos_msg)
-        #self.temp_count += 1
-        #self.get_logger().info('Publicando temperatura objetivo: %d' % temperatura_msg.data)
+        temperatura_cama_msg = Int32()
+        imprimir_msg = Int32()
+        longitud_msg = Int32()
+
+        temperatura_msg.data = self.temperatura_objetivo
+        temperatura_cama_msg.data = self.temperatura_cama_objetivo
+        imprimir_msg.data = self.imprimir_objetivo
+        longitud_msg.data = self.longitud_extrusion_objetivo
+
+        self.publisher_temperatura_objetivo.publish(temperatura_msg)
+        self.publisher_temperatura_cama_objetivo.publish(temperatura_cama_msg)
+        self.publisher_imprimir_objetivo.publish(imprimir_msg)
+        self.publisher_longitud_extrusion_objetivo.publish(longitud_msg)
+
 
     def temperatura_callback(self, msg):
+        self.get_logger().info('Temperatura enviada: %d' % self.temperatura_objetivo)
         self.get_logger().info('Temperatura recibida: %d' % msg.data)
 
-    def pasos_callback(self, msg):
-        self.get_logger().info('Pasos recibidos: %d' % msg.data)
+    def temperatura_cama_callback(self, msg):
+        self.get_logger().info('Temperatura cama recibida: %d' % msg.data)
+
+    def imprimir_callback(self, msg):
+        self.get_logger().info('Imprimir recibido: %d' % msg.data)
 
 def main(args=None):
     rclpy.init(args=args)
--- micro_ros_arduino/examples/micro-ros_publisher_prueba/micro-ros_publisher_prueba.ino
+++ micro_ros_arduino/examples/micro-ros_publisher_prueba/micro-ros_publisher_prueba.ino
@@ -1,3 +1,29 @@
+/*
+Programa de arduino que se carga en la ESP-WROOM32 para comunicarse con
+ROS2. Crea un nodo en el que publica la temperatura actual del extrusor
+y si esta imprimiendo o no. También se suscribe para recibir la temperatura 
+objetivo y si tiene que imprimir en ese momento o no.
+El otro archivo que hace que se comuniquen está en la siguiente dirección:
+
+/home/alvaro/workspace/ros_ur_driver/src/Universal_Robots_ROS2_Driver/ur_bringup/launch/com_arduino.py
+
+-Para que comience la comunicación primero hay que descargarse micro_ros, y como 
+pone en los tutoriales lanzar en un terminal:
+
+ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0
+
+-Si da problemas de seguridad dar permisos al USB:
+
+sudo chmod 666 /dev/ttyUSB0
+
+-Finalmente lanzar el archivo .py para comunicarse:
+
+python3 /<poner_la_direccion>/com_arduino.py
+
+*/
+
+
+
 #include <micro_ros_arduino.h>
 #include <stdio.h>
 #include <rcl/rcl.h>
@@ -5,12 +31,47 @@
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 #include <std_msgs/msg/int32.h>
+#include <math.h>
 
-#define TIMER_TIMEOUT_MS 5000 // Tiempo de espera en milisegundos (5 segundos)
+#define TIMER_TIMEOUT_MS 3000 // Tiempo de espera en milisegundos (5 segundos)
+
+//STEPPER Y TERMOPAR///////////////////////////////////////////////////////
+#include <PID_v1.h>
+#define RELE 33 //como??(nano)
+#define tiempoCiclo 1000
+#define termistorPin 34
+#define termistorNominalRes 100000
+#define termistorNominalTemp 25
+#define termistorBValue 3950
+#define VoltageDividerResistor 1000
+#define LED_PIN 2
+
+
+double Setpoint, Input, Output;                                     // Define Variables 
+double Kp=40, Ki=3, Kd=500;                                         // Especifica parametros iniciales
+
+unsigned long respuestaUltimaTemperatura = 0;
+unsigned long lastPIDCalculation = 0;
+float prevTemperature = -9999.0;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+const int DIR = 26;
+const int STEP = 25;
+const int ENABLE = 27;
+const int M0 = 18, M1 = 19, M2 = 20;
+
+double x;
+int pos;
+
+float termistorRes = 0.0;
+float steinhart;
+/////////////////////////////////////////////////////////////////////////////
+
 
 // Variables de control del temporizador
 unsigned long last_msg_time = 0;
 bool timer_expired = false;
+int imp=0;
 
 rclc_support_t support;
 rcl_allocator_t allocator;
@@ -18,23 +79,31 @@ rcl_node_t node;
 
 //publisher
 rcl_publisher_t publisher;
-rcl_publisher_t publisher_temperatura;
-rcl_publisher_t publisher_pasos;       
+rcl_publisher_t publisher_temperatura; 
+rcl_publisher_t publisher_temperatura_cama; 
+rcl_publisher_t publisher_imprimir;      
 rclc_executor_t executor_pub;
 rcl_timer_t timer;
-std_msgs__msg__Int32 temperatura;
+std_msgs__msg__Int32 temperatura; //Temperatura actual de extrusor
 std_msgs__msg__Int32 msg;
-std_msgs__msg__Int32 pasos;
+std_msgs__msg__Int32 temperatura_cama; //Temperatura actual de la cama
+std_msgs__msg__Int32 imprimir; //1 SI SE ESTA IMPRIMIENDO, 0 SI NO 
+
 
 //subscriber
-rcl_subscription_t subscriber;
 rcl_subscription_t subscriber_temperatura;
+rcl_subscription_t subscriber_temperatura_cama;
+rcl_subscription_t subscriber_imprimir;
+rcl_subscription_t subscriber_longitud;
 rclc_executor_t executor_sub;
-std_msgs__msg__Int32 temperatura_objetivo;
-std_msgs__msg__Int32 pasos_objetivo;
+std_msgs__msg__Int32 temperatura_objetivo; //Temperatura a la que se tiene que poner el extrusor
+std_msgs__msg__Int32 longitud_extrusion_objetivo; //La longitud de extrusion, para calcular el paso
+std_msgs__msg__Int32 imprimir_objetivo; //Si hay que imprimir o no
+std_msgs__msg__Int32 temperatura_cama_objetivo; //Temperatura a la que se tiene que poner la cama
 
 
-#define LED_PIN 13
+
+
 
 #define RCCHECK(fn)                    \
     {                                  \
@@ -56,26 +125,46 @@ void error_loop()
     while (1)
     {
         digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-        delay(100);
+        delay(1000);
     }
 }
 
 ////FUNCIONES DE PUBLISHER Y SUBSCRIBER/////////////////////////////////
 
-void subscription_callback(const void * msgin)
-{  
-  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-    pasos.data=temperatura_objetivo.data;
-    // Reiniciar el temporizador
-    last_msg_time = millis();
-    timer_expired = false;
-}
-
 void temperatura_callback(const void * msgin)
 {  
   const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
-  // Realizar acciones con el mensaje de temperatura
+    //pasos.data=temperatura_objetivo.data;
+    // Reiniciar el temporizador
+    last_msg_time = millis();
+    timer_expired = false;
+    Setpoint=temperatura_objetivo.data;
 }
+
+/*void temperatura_cama_callback(const void * msgin)
+{  
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+    // Reiniciar el temporizador
+    //last_msg_time = millis();
+    //timer_expired = false;
+    temperatura_cama.data=temperatura_cama_objetivo.data;
+}*/
+
+void imprimir_callback(const void * msgin)
+{  
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+    // Reiniciar el temporizador
+    last_msg_time = millis();
+    timer_expired = false;
+    imprimir.data=imprimir_objetivo.data;
+    imp=imprimir_objetivo.data;
+}
+
+/*void longitud_extrusion_callback(const void * msgin)
+{  
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+  // Realizar acciones con el mensaje de extrusion
+}*/
 
 //Funcion para publicar cada segundo, y en caso de dejar de recibir 
 //información de temperatura y pasos objetivo, los establece a 0, para
@@ -85,22 +174,50 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
     RCLC_UNUSED(last_call_time);
     if (timer != NULL)
     {
-        RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+        //RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
         RCSOFTCHECK(rcl_publish(&publisher_temperatura, &temperatura, NULL)); // Publicar temperatura
-        RCSOFTCHECK(rcl_publish(&publisher_pasos, &pasos, NULL));             // Publicar pasos
-        msg.data++;
-        temperatura.data++;
+        //RCSOFTCHECK(rcl_publish(&publisher_temperatura_cama, &temperatura_cama, NULL));           
+        //RCSOFTCHECK(rcl_publish(&publisher_imprimir, &imprimir, NULL));             
+
         // Comprobar si el temporizador ha expirado y detener impresión
         if (millis() - last_msg_time >= TIMER_TIMEOUT_MS)
         {
             // Establecer la temperatura y pasos objetivo a 0
             temperatura_objetivo.data = 0;
-            pasos_objetivo.data = 0;
-            temperatura.data = temperatura.data - 2;//prueba
+            imprimir_objetivo.data = 0;
+            imprimir.data=0;
+            temperatura_cama_objetivo.data=0;
+            longitud_extrusion_objetivo.data=0;
+            Setpoint=0;
+            imp=0;
+            digitalWrite(ENABLE,HIGH);//Apago el motor
             // Marcar que el temporizador ha expirado
             timer_expired = true;
         }
     }
+}
+
+void leerT(){
+  termistorRes = ((float)analogRead (termistorPin)* VoltageDividerResistor)/(4095 - (float)analogRead (termistorPin));
+  steinhart = termistorRes / termistorNominalRes;     // (R/Ro)
+  steinhart = log(steinhart);                         // ln(R/Ro)
+  steinhart /= termistorBValue;                       // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (termistorNominalTemp + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                        // Invert  
+  steinhart -= 273.15;                                // convert to C
+
+  temperatura.data=round(steinhart);
+}
+
+void control() {
+
+    if ((millis() <= (lastPIDCalculation + Output)) || (Output == tiempoCiclo)) {
+    // Power on:
+    digitalWrite(RELE, HIGH);
+  } else {
+    // Power off:
+    digitalWrite(RELE, LOW);
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -112,7 +229,28 @@ void setup()
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, HIGH);
 
-    delay(2000);
+    //STEPPER Y TERMOPAR/////////////////////////////////////////////////////////////////////////
+    pinMode(STEP, OUTPUT);
+    pinMode(DIR, OUTPUT);
+    pinMode(RELE, OUTPUT); 
+    pinMode(ENABLE, OUTPUT);
+    pinMode(M0, OUTPUT);
+    pinMode(M1, OUTPUT); 
+    pinMode(M2, OUTPUT);
+
+    //  stepper.setMaxSpeed(1000); //velocidad maxima permitid
+    //  stepper.setAcceleration(30);
+    //  stepper.setSpeed(1000);// relacion entre aceleracion/desaceleracion
+
+    Setpoint = 0; 
+    myPID.SetOutputLimits(0, tiempoCiclo);                           
+    myPID.SetSampleTime(tiempoCiclo);
+    myPID.SetMode(AUTOMATIC); 
+    digitalWrite(DIR,HIGH);
+    digitalWrite(ENABLE,HIGH);//Apago el motor
+    digitalWrite(M0,HIGH);
+    digitalWrite(M1,HIGH);
+    digitalWrite(M2,LOW);
 
     allocator = rcl_get_default_allocator();
 
@@ -123,36 +261,59 @@ void setup()
     RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
 
     // CREATE PUBLISHER////////////////////////////////////////
-    RCCHECK(rclc_publisher_init_default(
+    /*RCCHECK(rclc_publisher_init_default(
         &publisher,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
         "micro_ros_arduino_node_publisher"));//nombre del topic ****para lo que se quiera
-
+    */
     RCCHECK(rclc_publisher_init_default(
         &publisher_temperatura, 
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
         "temperatura"));//nombre del topic en el que se publica la temperatura del termopar
 
-    RCCHECK(rclc_publisher_init_default(
-        &publisher_pasos, 
+    /*RCCHECK(rclc_publisher_init_default(
+        &publisher_temperatura_cama, 
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-        "pasos"));//nombre del topic en el que se publican los pasos que se detectan en el extrusor
+        "temperatura_cama"));//nombre del topic en el que se publica la temp de la cama
+    RCCHECK(rclc_publisher_init_default(
+        &publisher_imprimir, 
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "imprimir"));//nombre del topic en el que se publica si hay que imprimir o no*/
   ///////////////////////////////////////////////////////////////
 
     // CREATE SUBSCRIBER //////////////////////////////////////
     RCCHECK(rclc_subscription_init_default(
-        &subscriber,
+        &subscriber_temperatura,
         &node,
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
         "temperatura_objetivo"));//nombre del topic que indica la temp objetivo
+    
+    /*RCCHECK(rclc_subscription_init_default(
+        &subscriber_temperatura_cama,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "temperatura_cama_objetivo"));//nombre del topic que indica la temp objetivo*/
+    
+    RCCHECK(rclc_subscription_init_default(
+        &subscriber_imprimir,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "imprimir_objetivo"));//nombre del topic que indica la temp objetivo
+
+    /*RCCHECK(rclc_subscription_init_default(
+        &subscriber_longitud,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "longitud_extrusion_objetivo"));//nombre del topic que indica la temp objetivo*/
 
   ///////////////////////////////////////////////////////////////
 
     // create timer,
-    const unsigned int timer_timeout = 1000;
+    const unsigned int timer_timeout = 100;
     RCCHECK(rclc_timer_init_default(
         &timer,
         &support,
@@ -163,22 +324,56 @@ void setup()
     RCCHECK(rclc_executor_init(&executor_pub, &support.context, 1, &allocator));
     RCCHECK(rclc_executor_add_timer(&executor_pub, &timer));
 
-    RCCHECK(rclc_executor_init(&executor_sub, &support.context, 1, &allocator));
-    RCCHECK(rclc_executor_add_subscription(&executor_sub, &subscriber, &temperatura_objetivo, &subscription_callback, ON_NEW_DATA));
-    //RCCHECK(rclc_executor_add_subscription(&executor, &subscriber_temperatura, &temperatura_objetivo, &temperatura_callback, ON_NEW_DATA));
+    RCCHECK(rclc_executor_init(&executor_sub, &support.context, 2, &allocator));//el 4 es el numero de ejecutores que maneja
+    RCCHECK(rclc_executor_add_subscription(&executor_sub, &subscriber_temperatura, &temperatura_objetivo, &temperatura_callback, ON_NEW_DATA));
+   // RCCHECK(rclc_executor_add_subscription(&executor_sub, &subscriber_temperatura_cama, &temperatura_cama_objetivo, &temperatura_cama_callback, ON_NEW_DATA));
+    RCCHECK(rclc_executor_add_subscription(&executor_sub, &subscriber_imprimir, &imprimir_objetivo, &imprimir_callback, ON_NEW_DATA));
+    //RCCHECK(rclc_executor_add_subscription(&executor_sub, &subscriber_longitud, &longitud_extrusion_objetivo, &longitud_extrusion_callback, ON_NEW_DATA));
 
 
     msg.data = 0;
     temperatura.data = 0;
-    pasos.data = 0;
-    temperatura_objetivo.data = 0;
-    pasos_objetivo.data = 0;
+    temperatura_cama.data = 0;
+    imprimir.data=0;
 
+    temperatura_objetivo.data = 0;
+    temperatura_cama_objetivo.data = 0;
+    longitud_extrusion_objetivo.data=0;
+    imprimir_objetivo.data=0;
+
+    delay(2000);
 }
 
 void loop()
 {
-    delay(100);
-    RCSOFTCHECK(rclc_executor_spin_some(&executor_pub, RCL_MS_TO_NS(100)));
-    RCCHECK(rclc_executor_spin_some(&executor_sub, RCL_MS_TO_NS(100)));
+    //delay(100);
+    //RCSOFTCHECK(rclc_executor_spin_some(&executor_pub, RCL_MS_TO_NS(100)));
+    RCSOFTCHECK(rclc_executor_spin_some(&executor_pub, 10));
+    RCCHECK(rclc_executor_spin_some(&executor_sub, 10));
+
+////STEPPER Y TERMOPAR////////////////////////////////////////////////////////////////
+
+    if(imp==1){
+        digitalWrite(STEP,HIGH);
+        delayMicroseconds(500);
+        digitalWrite(STEP,LOW);
+        delayMicroseconds(500);
+    }
+
+    if (millis() - respuestaUltimaTemperatura >= tiempoCiclo) {
+        leerT();
+        Input = steinhart;
+        myPID.Compute();
+        lastPIDCalculation = millis();
+        respuestaUltimaTemperatura = millis();
+    }
+
+    if(imp==0){
+        digitalWrite(ENABLE,HIGH);//Apago el motor
+    }
+    if(imp==1){
+        digitalWrite(ENABLE,LOW);//Activo el motor
+    }
+
+    control();
 }
